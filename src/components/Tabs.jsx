4f045fcd181f29af34ab1ba5e59dcfd5781dcc38
Ramil <Ramilmys@gmail.com>
2025-11-19 18:15:32 +0700

    В процессе

---
+++ src/components/Tabs.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+export default function Tabs() {
+  console.log("renderTasks");
+
+  const [activeTab, setActiveTab] = useState("Description");
+
+  const content = {
+    Description: <p>Вкладка с Description</p>,
+    Reviews: <p>Чето с Reviews</p>,
+    Specifications: <p>Какие-то Specifications</p>,
+  };
+
+  return (
+    <div className="tasks">
+      <h2>{activeTab}</h2>
+      {content[activeTab]}
+      <div className="btn-container">
+        <button
+          className={activeTab === "Description" ? "active-btn" : "btn"}
+          onClick={() => setActiveTab("Description")}
+        >
+          Description
+        </button> 
+        <button
+          className={activeTab === "Reviews" ? "active-btn" : "btn"}
+          onClick={() => setActiveTab("Reviews")}
+        >
+          Reviews
+        </button>
+        <button
+          className={activeTab === "Specifications" ? "active-btn" : "btn"}
+          onClick={() => setActiveTab("Specifications")}
+        >
+          Specifications
+        </button>
+      </div>
+    </div>
+  );
+}
