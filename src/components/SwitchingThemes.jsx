d5a38630c48ba9abc7c5d48099ed98f341e0544f
Ramil <Ramilmys@gmail.com>
2025-11-24 23:36:12 +0700

    Задча 5 решена

--- src/components/SwitchingThemes.jsx
+++ src/components/SwitchingThemes.jsx
@@ -1,62 +1,58 @@
 import React, { useState } from "react";
 import styles from "../styles/switching-themes.module.css";
 
+const son = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <circle cx="12" cy="12" r="4" />
+    <path d="M12 2v2" />
+    <path d="M12 20v2" />
+    <path d="m4.93 4.93 1.41 1.41" />
+    <path d="m17.66 17.66 1.41 1.41" />
+    <path d="M2 12h2" />
+    <path d="M20 12h2" />
+    <path d="m6.34 17.66-1.41 1.41" />
+    <path d="m19.07 4.93-1.41 1.41" />
+  </svg>
+);
+
+const moon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
+  </svg>
+);
+
+const content = {
+  Description: <p>Вкладка с Description</p>,
+  Reviews: <p>Чето с Reviews</p>,
+  Specifications: <p>Какие-то Specifications</p>,
+};
+
 export default function SwitchingThemes() {
   console.log("renderTasks");
 
   const [activeTab, setActiveTab] = useState("Description");
 
   const [switchTheme, setSwitchTheme] = useState("dark-theme");
-
-  const son = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="12" cy="12" r="4" />
-      <path d="M12 2v2" />
-      <path d="M12 20v2" />
-      <path d="m4.93 4.93 1.41 1.41" />
-      <path d="m17.66 17.66 1.41 1.41" />
-      <path d="M2 12h2" />
-      <path d="M20 12h2" />
-      <path d="m6.34 17.66-1.41 1.41" />
-      <path d="m19.07 4.93-1.41 1.41" />
-    </svg>
-  );
-
-  const moon = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M20.985 12.486a9 9 0 1 1-9.473-9.472c.405-.022.617.46.402.803a6 6 0 0 0 8.268 8.268c.344-.215.825-.004.803.401" />
-    </svg>
-  );
-
-  function switchingTheme() {
-    setSwitchTheme(switchTheme === "light-theme" ? "dark-theme" : "light-theme");
-  }
-
-  const content = {
-    Description: <p>Вкладка с Description</p>,
-    Reviews: <p>Чето с Reviews</p>,
-    Specifications: <p>Какие-то Specifications</p>,
-  };
 
   return (
     <div className={`${styles["switching-themes"]} ${styles[switchTheme]}`}>
@@ -101,7 +97,9 @@ export default function SwitchingThemes() {
         </button>
       </div>
       <button
-        onClick={switchingTheme}
+        onClick={() =>
+          setSwitchTheme(switchTheme === "light-theme" ? "dark-theme" : "light-theme")
+        }
         className={`${
           switchTheme === "light-theme"
             ? styles["dark-theme_btn-switching-themes"]
