bf8fc39d67e6b241a0026fcbeae688f58f42bd9a
Ramil <Ramilmys@gmail.com>
2025-11-27 04:13:49 +0700

    Задача 7 решена

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/counter.module.css";
 
 export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const [stepSize, setStepSize] = useState(0);
+
   return (
     <div className={styles["counter"]}>
       <h1>Counter</h1>
       <div className={styles["main"]}>
-        <input className={styles["input-steps"]} type="text" />
+        <input
+          placeholder="Enter the step size..."
+          className={styles["input-steps"]}
+          type="number"
+          value={stepSize}
+          onChange={(e) => setStepSize(e.target.value)}
+        />
         <div className={styles["btn-container"]}>
-          <button className={styles["btn-increment"]}>Increment</button>
-          <button className={styles["btn-decrement"]}>Decrement</button>
+          <button
+            onClick={() => setCount(count + +stepSize)}
+            className={styles["btn-decrement"]}
+          >
+            Decrement
+          </button>
+
+          <div>{count}</div>
+
+          <button
+            onClick={() => setCount(count - +stepSize)}
+            className={styles["btn-increment"]}
+          >
+            Increment
+          </button>
         </div>
       </div>
     </div>
