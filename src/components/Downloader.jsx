6e3837c2accc64e61245981fcb4272dfad98903e
Ramil <Ramilmys@gmail.com>
2025-11-28 03:34:09 +0700

    Задча 9 решена

---
+++ src/components/Downloader.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import styles from "../styles/downloader.module.css";
+
+export default function Downloader() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  function showDownload() {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }
+
+  return (
+    <div className={styles["downloader"]}>
+      <h1>Downloader</h1>
+      <div className={styles["main"]}>
+        <button onClick={showDownload} className={styles["btn"]} disabled={isLoading}>
+          {isLoading === true ? "Downloading..." : "Download"}
+        </button>
+      </div>
+    </div>
+  );
+}
