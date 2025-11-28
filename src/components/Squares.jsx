f9d02806ea3d0c7b91a870088f5012583407d1b9
Ramil <Ramilmys@gmail.com>
2025-11-29 01:49:50 +0700

    Задача 10 решена

--- src/components/Squares.jsx
+++ src/components/Squares.jsx
@@ -2,28 +2,28 @@ import React, { useState } from "react";
 import styles from "../styles/squares.module.css";
 
 function generatedRandomColor() {
-  return Math.random().toString(16).substring(2, 8);
+  return `#${Math.random().toString(16).substring(2, 8)}`;
 }
 
 export default function Squares() {
   const [squares, setSquares] = useState([
-    { id: 1, color: `#${generatedRandomColor()}` },
-    { id: 2, color: `#${generatedRandomColor()}` },
+    { id: 1, color: generatedRandomColor() },
+    { id: 2, color: generatedRandomColor() },
   ]);
 
   function addSquare() {
     const newSquare = {
       id: Date.now(),
-      color: `#${generatedRandomColor()}`,
+      color: generatedRandomColor(),
     };
 
     setSquares([...squares, newSquare]);
   }
 
-  function askRandomColor(id) {
+  function setRandomColor(id) {
     setSquares(
       squares.map((square) =>
-        square.id === id ? { ...square, color: `#${generatedRandomColor()}` } : square
+        square.id === id ? { ...square, color: generatedRandomColor() } : square
       )
     );
   }
@@ -39,7 +39,7 @@ export default function Squares() {
           {squares.map((square) => (
             <div
               key={square.id}
-              onClick={() => askRandomColor(square.id)}
+              onClick={() => setRandomColor(square.id)}
               style={{ backgroundColor: square.color }}
               className={styles["square"]}
             ></div>
