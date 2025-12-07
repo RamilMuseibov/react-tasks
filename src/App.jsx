130a1364e3feeea01d06548137f14041dea53aef
Ramil <Ramilmys@gmail.com>
2025-12-08 04:54:07 +0700

    Все компоненты импортированы

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
-import Tabs from "./components/Tabs";
-import Converter from "./components/Converter";
-import Accordion from "./components/Accordion";
-import Slider from "./components/Slider";
-import SwitchingThemes from "./components/SwitchingThemes";
-import Password from "./components/Password";
-import Counter from "./components/Counter";
-import Volume from "./components/Volume";
-import Downloader from "./components/Downloader";
-import Squares from "./components/Squares";
-import Accordion2 from "./components/Accordion2.0";
-import Multilingualism from "./components/Multilingualism";
-import PaginatedList, { ProductCard, UserCard } from "./components/FetchUsers";
-import Tooltip from "./components/Tooltip";
+import UseStateTasks from "./components/UseStateTasks";
 
 function App() {
   return (
     <>
-      <Tabs />
-      <Converter />
-      <Accordion />
-      <Slider />
-      <SwitchingThemes />
-      <Password />
-      <Counter />
-      <Volume />
-      <Downloader />
-      <Squares />
-      <Accordion2 />
-      <Multilingualism />
-      <PaginatedList entity="users" entityCard={UserCard} />
-      <PaginatedList entity="products" entityCard={ProductCard} />
-      <Tooltip />
+      <UseStateTasks />
     </>
   );
 }
