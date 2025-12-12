ea1ba445b7c282797bafa99bb8bdb8c0b46d615c
Ramil <Ramilmys@gmail.com>
2025-12-13 02:13:23 +0700

    Задача в процессе редактирования

---
+++ src/components/use-effect-tasks/TitleUpdater.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../../styles/use-effect-tasks/title-updater.module.css";
+
+export default function TitleUpdater() {
+  return (
+    <div className={styles["title-updater"]}>
+      <h1>TitleUpdater</h1>
+    </div>
+  );
+}
