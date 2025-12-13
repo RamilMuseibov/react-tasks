ab7618388f7e426075c1760128bf3374a8543c29
Ramil <Ramilmys@gmail.com>
2025-12-14 04:41:56 +0700

    Задача решена

---
+++ src/components/use-effect-tasks/CurrentTime.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import styles from "../../styles/use-effect-tasks/current-time.module.css";
+
+export default function CurrentTime() {
+  const [times, setTimes] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    setInterval(() => {
+      setTimes(new Date().toLocaleTimeString());
+    }, 1000);
+  }, []);
+
+  return (
+    <div className={styles["current-time"]}>
+      <h1>CurrentTime</h1>
+      <div>{times}</div>
+    </div>
+  );
+}
