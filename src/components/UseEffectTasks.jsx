de6bacaf5b01ee217fb4e12a1d7d1370b2f90868
Ramil <Ramilmys@gmail.com>
2025-12-14 03:27:03 +0700

    Главный файл компонентов UseEffect задач

--- src/components/UseEffectTasks.jsx
+++ src/components/UseEffectTasks.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import UserList from "./use-effect-tasks/UserList";
 import TitleUpdater from "./use-effect-tasks/TitleUpdater";
 import TrackingCounter from "./use-effect-tasks/TrackingCounter";
+import ProductFilter from "./use-effect-tasks/ProductFilter";
 
 export default function UseEffectTasks() {
   return (
     <>
       {/* <UserList /> */}
       {/* <TitleUpdater /> */}
-      <TrackingCounter />
+      {/* <TrackingCounter /> */}
+      <ProductFilter />
     </>
   );
 }
