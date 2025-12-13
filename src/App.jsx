f1b658ad6ee04affa22fe6fbacd0da0fb61ae9b0
Ramil <Ramilmys@gmail.com>
2025-12-13 07:18:27 +0700

    Все компоненты импортированы

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import UseEffectTasks from "./components/UseEffectTasks";
 import UseStateTasks from "./components/UseStateTasks";
 
 function App() {
-  console.log("какая-то хуйня");
 
   return (
     <>
