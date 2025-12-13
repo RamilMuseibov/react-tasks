d5f438a03c4f9cba8ae117ef03e26dafcc02e3dd
Ramil <Ramilmys@gmail.com>
2025-12-13 07:05:42 +0700

    Задача решена

--- src/components/use-effect-tasks/TitleUpdater.jsx
+++ src/components/use-effect-tasks/TitleUpdater.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../../styles/use-effect-tasks/title-updater.module.css";
 
 export default function TitleUpdater() {
+  const [text, setText] = useState("TitleUpdater");
+
+  useEffect(() => {
+    document.title = text;
+  }, [text]);
+
   return (
     <div className={styles["title-updater"]}>
       <h1>TitleUpdater</h1>
+
+      <input
+        onChange={(e) => setText(e.target.value)}
+        className={styles["input"]}
+        type="text"
+        placeholder="Ente title..."
+      />
     </div>
   );
 }
