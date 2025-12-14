9a990f8d586c1347099b2a5fa26f8c2b70f4e3cc
Ramil <Ramilmys@gmail.com>
2025-12-15 00:26:24 +0700

    Главный файл компонентов UseEffect задач

--- src/components/UseEffectTasks.jsx
+++ src/components/UseEffectTasks.jsx
@@ -9,10 +9,10 @@ export default function UseEffectTasks() {
   return (
     <>
       {/* <UserList /> */}
-      {/* <TitleUpdater /> */}
+      <TitleUpdater />
       {/* <TrackingCounter /> */}
       {/* <ProductFilter /> */}
-      <CurrentTime />
+      {/* <CurrentTime /> */}
     </>
   );
 }
