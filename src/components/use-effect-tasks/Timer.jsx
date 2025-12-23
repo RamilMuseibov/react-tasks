3e6f97045408ba8add9b800287b853a5c4e6838a
Ramil <Ramilmys@gmail.com>
2025-12-24 05:14:00 +0700

    Задача решена

--- src/components/use-effect-tasks/Timer.jsx
+++ src/components/use-effect-tasks/Timer.jsx
@@ -2,53 +2,66 @@ import React, { useEffect, useState } from "react";
 import styles from "../../styles/use-effect-tasks/timer.module.css";
 
 export default function Timer() {
-  // const [timer, setTimer] = useState({ hours: 0, minutes: 0, seconds: 0 });
+  const [timer, setTimer] = useState({ hours: 0, minutes: 0, seconds: 0 });
 
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
-  const [isActive, setIsActive] = useState(false);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
+    if (!isRunning) {
+      return;
+    }
+
     const idInteval = setInterval(() => {
-      if (isActive) {
+      setTimer((prev) => {
+        const { hours, minutes, seconds } = prev;
+
         if (seconds > 0) {
-          setSeconds(seconds - 1);
-        } else {
-          if (minutes > 0) {
-            setMinutes(minutes - 1);
-            setSeconds(59);
-          } else {
-            if (hours > 0) {
-              setHours(hours - 1);
-              setMinutes(59);
-              setSeconds(59);
-            } else {
-              setIsActive(false);
-            }
-          }
+          return { hours, minutes, seconds: seconds - 1 };
+          // return { ...prev, seconds: seconds - 1 };
         }
-      }
+
+        if (minutes > 0) {
+          return { hours, minutes: minutes - 1, seconds: 59 };
+        }
+
+        if (hours > 0) {
+          return { hours: hours - 1, minutes: 59, seconds: 59 };
+        }
+
+        setIsRunning(false);
+
+        return { hours: 0, minutes: 0, seconds: 0 };
+      });
     }, 1000);
 
     return () => {
       clearInterval(idInteval);
     };
-  }, [isActive, seconds]);
+  }, [isRunning]);
 
   function clearTimer() {
-    setHours(0);
-    setMinutes(0);
-    setSeconds(0);
-    setIsActive(false);
+    setTimer({ hours: 0, minutes: 0, seconds: 0 });
+    setIsRunning(false);
+  }
+
+  function updateSeconds(inputValue) {
+    setTimer({ ...timer, seconds: inputValue > 59 ? 59 : inputValue });
+  }
+
+  function updateMinutes(inputValue) {
+    setTimer({ ...timer, minutes: inputValue > 59 ? 59 : inputValue });
+  }
+
+  function updateHours(inputValue) {
+    setTimer({ ...timer, hours: inputValue > 23 ? 23 : inputValue });
   }
 
   return (
     <div className={styles["timer"]}>
       <h1>Timer</h1>
       <div className={styles["btn-container"]}>
-        <button onClick={() => setIsActive(true)} className={styles["btn-launch"]}>
-          Start
+        <button onClick={() => setIsRunning(!isRunning)} className={styles["btn-launch"]}>
+          {isRunning ? "Pause" : "Start"}
         </button>
         <button onClick={clearTimer} className={styles["btn-clear"]}>
           Clear
@@ -57,22 +70,22 @@ export default function Timer() {
       <div className={styles["timer_input-container"]}>
         <input
           className={styles["input-hours"]}
-          onChange={(e) => setHours(+e.target.value > 23 ? 23 : +e.target.value)}
-          value={hours < 10 ? `0${hours}` : hours.toString()}
+          onChange={(e) => updateHours(+e.target.value)}
+          value={timer.hours < 10 ? `0${timer.hours}` : timer.hours.toString()}
           type="number"
         />
         :
         <input
           className={styles["input-minutes"]}
-          onChange={(e) => setMinutes(+e.target.value > 59 ? 59 : +e.target.value)}
-          value={minutes < 10 ? `0${minutes}` : minutes.toString()}
+          onChange={(e) => updateMinutes(+e.target.value)}
+          value={timer.minutes < 10 ? `0${timer.minutes}` : timer.minutes.toString()}
           type="number"
         />
         :
         <input
           className={styles["input-seconds"]}
-          onChange={(e) => setSeconds(+e.target.value > 59 ? 59 : +e.target.value)}
-          value={seconds < 10 ? `0${seconds}` : seconds.toString()}
+          onChange={(e) => updateSeconds(+e.target.value)}
+          value={timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds.toString()}
           type="number"
         />
       </div>
