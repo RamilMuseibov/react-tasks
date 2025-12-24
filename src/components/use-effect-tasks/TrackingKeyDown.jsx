57848ffbec0cc030c1ec36da4a416652886e1aa4
Ramil <Ramilmys@gmail.com>
2025-12-24 22:54:21 +0700

    Задача решена

--- src/components/use-effect-tasks/TrackingKeyDown.jsx
+++ src/components/use-effect-tasks/TrackingKeyDown.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 
 export default function TrackingKeyDown() {
-  const [keydown, setKeydown] = useState("");
+  const [keydown, setKeydown] = useState([]);
+  console.log(keydown);
 
   useEffect(() => {
-    const keydown = (e) => setKeydown(e.key);
+    const keydownF = (e) => setKeydown((prev) => [...prev, e.key]);
 
-    document.addEventListener("keydown", keydown);
+    document.addEventListener("keydown", keydownF);
 
     return () => {
-      document.removeEventListener("keydown", keydown);
+      document.removeEventListener("keydown", keydownF);
     };
   }, []);
 
   return (
     <div style={{ padding: 10 }}>
       <h1>TrackingKeyDown</h1>
-      <div>{`Вы нажали клавишу: ${keydown}`}</div>
+      {keydown.slice(-10).map((key, index) => {
+        return <div key={index}>{`Вы нажали клавишу: ${key}`}</div>;
+      })}
     </div>
   );
 }
