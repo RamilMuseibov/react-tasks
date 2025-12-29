336af2a16e8a682da52fd7914fbe77daf04b709f
Ramil <Ramilmys@gmail.com>
2025-12-30 03:47:48 +0700

    Все компоненты импортированы

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import CompositionTasks from "./components/CompositionTasks";
 import UseEffectTasks from "./components/UseEffectTasks";
+import UseRefTasks from "./components/UseRefTasks";
 import UseStateTasks from "./components/UseStateTasks";
 
 function App() {
-
   return (
     <>
       {/* <UseStateTasks /> */}
       {/* <CompositionTasks /> */}
-      <UseEffectTasks />
+      {/* <UseEffectTasks /> */}
+      <UseRefTasks />
     </>
   );
 }
