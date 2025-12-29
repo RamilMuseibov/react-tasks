807ea227fcfeb23cb788fc3fa380425efc9dc478
Ramil <Ramilmys@gmail.com>
2025-12-30 03:47:13 +0700

    Главный файл компонентов UseRef

---
+++ src/components/UseRefTasks.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import InputFocus from "./use-ref-tasks/InputFocus";
+
+export default function UseRefTasks() {
+  return (
+    <>
+      <InputFocus />
+    </>
+  );
+}
