f4f2ca1b26d66cb5a36b4a8d1374379719bef057
Ramil <Ramilmys@gmail.com>
2026-01-01 06:17:07 +0700

    Задача решена

---
+++ src/components/use-ref-tasks/QuantityRenders.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef, useState } from "react";
+import styles from "../../styles/use-ref-tasks/quantity-renders.module.css";
+
+export default function QuantityRenders() {
+  const quantityRenders = useRef(0);
+  quantityRenders.current++;
+
+  console.log("render");
+
+  const displayQuantityRenders = useRef(null);
+
+  const [times, setTimes] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTimes(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
+  return (
+    <div className={styles["quantity-renders"]}>
+      <h1>QuantityRenders</h1>
+      <div>{times}</div>
+
+      <div className={styles["quantity-renders-container"]}>
+        QuantityRenders:
+        <div ref={displayQuantityRenders}>{quantityRenders.current}</div>
+      </div>
+    </div>
+  );
+}
