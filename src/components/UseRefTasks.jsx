e0728d7f5fd3d675b8611e24dab2ad7ac13775c7
Ramil <Ramilmys@gmail.com>
2026-01-02 06:10:13 +0700

    Главный файл компонентов UseRef

--- src/components/UseRefTasks.jsx
+++ src/components/UseRefTasks.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import InputFocus from "./use-ref-tasks/InputFocus";
 import CounterWithoutRender from "./use-ref-tasks/CounterWithoutRender";
+import PrevCountValue from "./use-ref-tasks/PrevCountValue";
+import QuantityRenders from "./use-ref-tasks/QuantityRenders";
+import SavingTaimer from "./use-ref-tasks/SavingTaimer";
+import ComparisonValues from "./use-ref-tasks/ComparisonValues";
+import ManagementDOMElement from "./use-ref-tasks/ManagementDOMElement";
+import LastClick from "./use-ref-tasks/LastClick";
+import FormWithoutRenders from "./use-ref-tasks/FormWithoutRenders";
 
 export default function UseRefTasks() {
   return (
     <>
       {/* <InputFocus /> */}
-      <CounterWithoutRender />
+      {/* <CounterWithoutRender /> */}
+      {/* <PrevCountValue /> */}
+      {/* <QuantityRenders /> */}
+      {/* <SavingTaimer /> */}
+      {/* <ComparisonValues /> */}
+      {/* <ManagementDOMElement /> */}
+      {/* <LastClick /> */}
+      <FormWithoutRenders />
     </>
   );
 }
