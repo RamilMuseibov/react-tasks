bde703fb5faa57817a4e7e2f0fdc04560118575c
Ramil <Ramilmys@gmail.com>
2026-01-04 01:00:00 +0700

    Задача решена

--- src/components/use-ref-tasks/FormWithoutRenders.jsx
+++ src/components/use-ref-tasks/FormWithoutRenders.jsx
@@ -4,64 +4,48 @@ import styles from "../../styles/use-ref-tasks/form-without-renders.module.css";
 export default function FormWithoutRenders() {
   console.log("render");
 
-  const dataUser = useRef({
-    firstName: "",
-    lastName: "",
-    login: "",
-    password: "",
-  });
-  console.log(dataUser.current);
+  const form = useRef();
 
+  console.log(new FormData());
 
   return (
     <div className={styles["form-without-renders"]}>
       <h1>FormWithoutRenders</h1>
 
-      <form className={styles["form-container"]}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(Object.fromEntries(new FormData(form.current)));
+        }}
+        ref={form}
+        className={styles["form-container"]}
+      >
         <label>
           <div>FirstName</div>
-          <input
-            onChange={(e) => (dataUser.current.firstName = e.target.value)}
-            defaultValue={dataUser.current.firstName}
-            type="text"
-            placeholder="Enter first name..."
-          />
+          <input name="firstName" type="text" placeholder="Enter first name..." />
         </label>
 
         <label>
           <div>LastName</div>
-          <input
-            onChange={(e) => (dataUser.current.lastName = e.target.value)}
-            defaultValue={dataUser.current.lastName}
-            type="text"
-            placeholder="Enter last name..."
-          />
+          <input name="lastName" type="text" placeholder="Enter last name..." />
         </label>
 
         <label>
           <div>Login</div>
-          <input
-            onChange={(e) => (dataUser.current.login = e.target.value)}
-            defaultValue={dataUser.current.login}
-            type="text"
-            placeholder="Enter login..."
-          />
+          <input name="login" type="text" placeholder="Enter login..." />
         </label>
 
         <label>
           <div>Password</div>
-          <input
-            onChange={(e) => (dataUser.current.password = e.target.value)}
-            defaultValue={dataUser.current.password}
-            type="text"
-            placeholder="Enter password..."
-          />
+          <input name="password" type="password" placeholder="Enter password..." />
         </label>
-      </form>
 
-      <button onClick={() => console.log(dataUser.current)} className={styles["btn"]}>
-        Send
-      </button>
+        <div>
+          <button type="submit" className={styles["btn"]}>
+            Send
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
