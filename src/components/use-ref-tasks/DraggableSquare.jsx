d60f6bebe91ad1ee1e037dcfb611a8b0ec68e1da
Ramil <Ramilmys@gmail.com>
2026-01-04 10:34:21 +0700

    В процессе

---
+++ src/components/use-ref-tasks/DraggableSquare.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../../styles/use-ref-tasks/draggable-square.module.css";
+
+export default function DraggableSquare() {
+  return (
+    <div className={styles["draggable-square"]}>
+      <h1>DraggableSquare</h1>
+    </div>
+  );
+}
